d8850172a7f0648d5a23ffd9ef05628c7bdacb8b
chenkun <chenkun@anmaicloud.com>
2021-05-26 18:30:20 +0800

    更新代码

--- components/issue-item.vue
+++ components/issue-item.vue
@@ -1,8 +1,19 @@
 <template>
   <div>
-    <div class="flex text-sm text-secondary">
-      <p>{{ this.$dayjs(this.issueItem.updatedAt).format("YYYY年 M月D日") }}</p>
-      <p></p>
+    <div class="flex items-center justify-start mb-3">
+      <div class="mr-4 text-base">
+        {{ this.$dayjs(issueItem.updatedAt).format("YYYY年 M月D日") }}
+      </div>
+      <div>
+        <div
+          class="p-0.5 text-white rounded-lg bg-opacity-50"
+          v-for="(value, index) in issueItem.labels.nodes"
+          :key="index"
+          :style="{ backgroundColor: '#' + value.color }"
+        >
+          {{ value.name }}
+        </div>
+      </div>
     </div>
     <h1>
       <NuxtLink :to="`/issue/${issueItem.number}`">
